6e0089afd55b5f0f1b3035f1cf4ce3022e5ce85f
joeydanielallen <jda144@hotmail.com>
2017-05-16 10:09:19 -0500

    adding roles

---
+++ LU.Web.Core/src/app/shared/models/application-roles.ts
@@ -0,0 +1,5 @@
+export class ApplicationRoles {
+    public readonly CreatorRead = 'CreatorRead';
+    public readonly CreatorUpdate = 'CreatorUpdate';
+    public readonly CreatorDelete = 'CreatorDelete';
+}
