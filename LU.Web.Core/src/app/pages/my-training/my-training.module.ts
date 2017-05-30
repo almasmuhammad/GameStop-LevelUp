35644ca87a94f40418e856cc6bb62bc049df0ccd
joeydanielallen <jda144@hotmail.com>
2017-05-30 12:01:16 -0500

    adding sub components for my-training, i18n for All Missions

--- LU.Web.Core/src/app/pages/my-training/my-training.module.ts
+++ LU.Web.Core/src/app/pages/my-training/my-training.module.ts
@@ -6,6 +6,9 @@ import { TranslateModule } from '@ngx-translate/core';
 import { MyTrainingRoutingModule } from './my-training-routing.module';
 
 import { MyTrainingComponent } from './my-training.component';
+import { MissionSectionComponent } from './components/mission-section/mission-section.component';
+import { ProfileSectionComponent } from './components/profile-section/profile-section.component';
+import { TrainingHistoryComponent } from './components/training-history/training-history.component';
 
 @NgModule({
   imports: [
@@ -14,7 +17,10 @@ import { MyTrainingComponent } from './my-training.component';
     MyTrainingRoutingModule
   ],
   declarations: [
-    MyTrainingComponent
+    MyTrainingComponent,
+    MissionSectionComponent,
+    ProfileSectionComponent,
+    TrainingHistoryComponent
   ]
 })
 export class MyTrainingModule { }
