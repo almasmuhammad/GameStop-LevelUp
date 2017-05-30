208717790968ced82c80e26c41b52b5b11acf7d7
joeydanielallen <jda144@hotmail.com>
2017-05-30 11:42:25 -0500

    my training, all missions i18n implemented

--- LU.Web.Core/src/app/pages/my-training/my-training.module.ts
+++ LU.Web.Core/src/app/pages/my-training/my-training.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TranslateModule } from '@ngx-translate/core';
+
 import { MyTrainingRoutingModule } from './my-training-routing.module';
 
 import { MyTrainingComponent } from './my-training.component';
@@ -8,6 +10,7 @@ import { MyTrainingComponent } from './my-training.component';
 @NgModule({
   imports: [
     CommonModule,
+    TranslateModule,
     MyTrainingRoutingModule
   ],
   declarations: [
